4fa3ff6c93d0002fe6f16ca28cd8e4f92eae8cec
Aurelie Tournay <info@aurelietournay.com>
2022-04-23 16:53:33 +0200

    update controller but not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Detroying all lists"
+List.destroy_all
+
+puts "Detroying all bookmarks"
+Bookmark.destroy_all
+
+puts "Detroying all movies"
+Movie.destroy_all
+
+puts "Creating new movies"
+10.times do
+  movie = Movie.create!(title: Faker::Movie.title,
+                        overview: Faker::Lorem.sentence,
+                        poster_url: Faker::Internet.url,
+                        rating: rand(1..10))
+  movie.save
+end
+
+puts "Creating new lists"
+List.create!(name: "Drama")
+List.create!(name: "Comedy")
+List.create!(name: "To rewatch")
+
+puts "Creating new bookmarks"
+Bookmark.create!(comment: "Recommended by John", movie: Movie.first, list: List.first)
